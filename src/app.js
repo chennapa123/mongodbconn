92a2ff85882e79205e0502e31f4461cc386f8165
chennapa123 <chennapanaidu_cherukuri@srmap.edu.in>
2026-06-16 15:43:05 +0530

    Add product routes to the application

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
 const userRoutes = require("./router/user.route");
+const productRoutes = require("./router/product.route");
 
 const app=express();
 
 app.use(express.json());
 
 app.use('/users', userRoutes);
+app.use('/products', productRoutes);
 
 module.exports = app;
